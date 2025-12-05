013e2f3031ec62cbca07f472693f42415aa1cf3a
Taiga Morishita <95189694+TaigaMorishita@users.noreply.github.com>
2025-12-04 19:01:59 -0500

    Add preheat and change heat algorithm

--- Reflow Oven/src/main.cpp
+++ Reflow Oven/src/main.cpp
@@ -5,6 +5,13 @@
 #include "Adafruit_ILI9341.h"
 #include "InterpolationLib.h"
 
+#define CALIBRATE_SECONDS 100
+#define CALIBRATE_TEMP 10
+
+#define DEBUG false
+
+#define PREHEAT_TEMP 75 // temperature to preheat around in degrees C
+
 #define THERMO_DAT 26
 #define THERMO_CLK 27
 #define THERMO_CS 28
@@ -23,14 +30,15 @@
 
 #define RELAYPIN 8
 #define STARTBTN 4
+#define PREHEATBTN 7
 #define BUZZERPIN 6
 
 // Datasheet settings
-const float timerange[] = {0.0, 280.0};
-const float temprange[] = {25.0, 235.0};
+const float timerange[] = {0.0, 280.0};             // min time, max time
+const float temprange[] = {25.0, 235.0};            // min temperature, max temperature
 double times[] = {0, 30, 120, 150, 210, 240, 280};  // interpolation requires this to be a non-constant double
 double temps[] = {25, 100, 150, 183, 235, 183, 25}; // interpolation requires this to be a non-constant double
-const float maxTemperature[] = {210.0, 235.0};
+const float maxTemperature[] = {210.0, 235.0};      // time of the max temp, max temperature
 
 // Adafruit's EZ Make Oven Settings
 // const float timerange[] = {0.0, 340.0};
@@ -55,10 +63,10 @@ const float PpDY = zeroY / (temprange[1] - temprange[0]);
 const float secondsPerPixel = 1 / PpSX;
 
 unsigned long int lastSwitch = 0; // last time the SSR was switched
-unsigned long int lastRead;       // last time the MAX6675 was read
-unsigned long int startTime;      // time of reflow start
-float targetTemperature = temps[0];
-bool switchState = false; // state of relay (true =  on)
+// unsigned long int lastRead;       // last time the MAX6675 was read
+unsigned long int startTime; // time of reflow start
+// float targetTemperature = temps[0];
+// bool switchState = false; // state of relay (true =  on)
 int currentStage = 1;
 bool stageDone = false;
 
@@ -66,7 +74,8 @@ const char *currentTemperatureChar[5];
 const char *targetTemperatureChar[5];
 
 void finishedReflow();
-float getTargetTemp();
+float getTargetTemp(int futureTime = 0);
+bool setRelay(bool, int tempToHold = 0);
 
 void setup()
 {
@@ -75,6 +84,7 @@ void setup()
   digitalWrite(RELAYPIN, LOW);
   pinMode(STARTBTN, INPUT_PULLUP);
   pinMode(BUZZERPIN, OUTPUT);
+  pinMode(PREHEATBTN, INPUT_PULLUP);
 
   Serial.begin(115200);
 
@@ -99,26 +109,15 @@ void setup()
   if (status != 0)
   {
     Serial.println(status);
+    tft.print("Thermcouple Error!");
+    while (1)
+    {
+      delay(25);
+    }
   }
   temperature = thermoCouple.getCelsius();
   Serial.println(temperature);
-  lastRead = millis();
-
-  // check for obvious thermocouple issues
-  // if (thermoCouple.getRawData() == 0)
-  // {
-  //   tft.print("Temperature  = 0 degrees C\nMAX6675 disconnected.\n(Or you're doing winter reflow?)\n\nHit start to bypass");
-  //   while (digitalRead(STARTBTN))
-  //   {
-  //   }
-  //   tft.fillScreen(ILI9341_BLACK);
-  //   delay(500);
-  // }
-  // else if (temperature < -274)
-  // {
-  //   tft.print("Temperature below -274 C.\nEither you're breaking physics or\nthe thermocouple is disconnected.\nEither way, please fix that.");
-  //   return;
-  // }
+  // lastRead = millis();
 
   // draw the key point lines
   int mappedTime;
@@ -170,8 +169,8 @@ void setup()
   // who says waiting is boring? This bit of code doesn't!
   int j = 0;
   long unsigned int lastDot = millis() - 1000;
-  while (digitalRead(STARTBTN))
-  { // wait until the button is pressed
+  while (digitalRead(STARTBTN) && digitalRead(PREHEATBTN))
+  { // wait until either button is pressed
     if (millis() > lastDot + 1000)
     {
       tft.setCursor(0, SCREEN_HEIGHT - TEXT_HEIGHT);
@@ -195,96 +194,98 @@ void setup()
     }
   }
 
-  // we're good to go! Find out whether to switch the SSR to start
+  while (digitalRead(STARTBTN)) // preheat, waiting for actual start
+  {
+    thermoCouple.read();
+    temperature = thermoCouple.getCelsius();
+    setRelay(true, PREHEAT_TEMP);
+    tft.setCursor(0, SCREEN_HEIGHT - TEXT_HEIGHT);
+    tft.print("Preheat: ");
+    tft.print(temperature);
+  }
+
+  // temperature = thermoCouple.getCelsius();
+
+  // // if you're somehow below room temp, bring it up to room temp before starting the clock
+  // if (temperature < temps[0])
+  // {
+  //   tft.print("Below Room Temp...");
+  //   digitalWrite(RELAYPIN, HIGH);
+  //   switchState = true;
+  //   lastSwitch = millis();
+  //   temperature = thermoCouple.getCelsius();
+  //   delay(READ_DELAY);
+  // }
+
+  // targetTemperature = temps[1];
+  // if (temperature < targetTemperature)
+  // {
+  //   digitalWrite(RELAYPIN, HIGH);
+  //   switchState = true;
+  //   lastSwitch = millis();
+  // }
+
+  // lastRead = millis();
+
+  // we're good to go!
   tft.fillRect(0, SCREEN_HEIGHT - TEXT_HEIGHT, SCREEN_WIDTH, TEXT_HEIGHT, ILI9341_BLACK);
   tft.setCursor(0, SCREEN_HEIGHT - TEXT_HEIGHT);
-  temperature = thermoCouple.getCelsius();
 
-  // if you're somehow below room temp, bring it up to room temp before starting the clock
-  if (temperature < temps[0])
-  {
-    tft.print("Below Room Temp...");
-    digitalWrite(RELAYPIN, HIGH);
-    switchState = true;
-    lastSwitch = millis();
-    temperature = thermoCouple.getCelsius();
-    delay(READ_DELAY);
-  }
-
-  targetTemperature = temps[1];
-  if (temperature < targetTemperature)
-  {
-    digitalWrite(RELAYPIN, HIGH);
-    switchState = true;
-    lastSwitch = millis();
-  }
-
-  lastRead = millis();
-  startTime = lastRead;
+  startTime = millis();
 }
 
 void loop()
 {
-  if (millis() > lastRead + READ_DELAY)
+  thermoCouple.read();
+  temperature = thermoCouple.getCelsius();
+
+  // TODO: fix this
+  if (temperature > getTargetTemp())
   {
-    thermoCouple.read();
-    temperature = thermoCouple.getCelsius();
-    targetTemperature = getTargetTemp();
-    if (temperature > targetTemperature)
+    stageDone = true;
+  }
+  if (stageDone && (millis() - startTime) / 1000 > times[currentStage])
+  {
+    stageDone = false;
+    currentStage++;
+    if (currentStage >= arrayLen - 1 && (millis() - startTime) / 1000 >= timerange[1])
     {
-      stageDone = true;
+      finishedReflow();
     }
-    if (stageDone && (millis() - startTime) / 1000 > times[currentStage])
-    {
-      stageDone = false;
-      currentStage++;
-      if (currentStage >= arrayLen - 1 && (millis() - startTime) / 1000 >= timerange[1])
-      {
-        finishedReflow();
-      }
-    }
-    lastRead = millis();
-
-    // show the temperatures on the screen
-    tft.setCursor(5 * .6 * TEXT_HEIGHT, SCREEN_HEIGHT - TEXT_HEIGHT);
-    tft.print(temperature);
-    tft.print("/");
-    tft.print((int)targetTemperature);
-    tft.write(0xF8);
-    tft.print("C");
-
-    // dtostrf(temperature, 5, 1, &currentTemperatureChar);
-
-    // also print it to serial for debug purposes
-    Serial.print((millis() - startTime) / 1000);
-    Serial.print("\tTemperature: ");
-    Serial.print(temperature);
-    Serial.print("\tTarget: ");
-    Serial.print(targetTemperature);
-    Serial.print("\tStage: ");
-    Serial.print(currentStage);
-    Serial.print("\t");
-    Serial.println(switchState);
   }
 
-  // if enough time has passed since the last time we switched AND a switch is needed, make it
-  if (targetTemperature > temperature && !switchState && millis() > lastSwitch + SWITCH_DELAY)
-  {
-    digitalWrite(RELAYPIN, HIGH);
-    switchState = true;
-    lastSwitch = millis();
-  }
-  else if (targetTemperature < temperature && switchState && millis() > lastSwitch + SWITCH_DELAY)
-  {
-    digitalWrite(RELAYPIN, LOW);
-    switchState = false;
-    lastSwitch = millis();
-  }
+  // if (temperature > maxTemperature[1] && millis() - startTime > maxTemperature[0])
+  // {
+  //   finishedReflow();
+  // }
+
+  // show the temperatures on the screen
+  tft.setCursor(5 * .6 * TEXT_HEIGHT, SCREEN_HEIGHT - TEXT_HEIGHT);
+  tft.print(temperature);
+  tft.print("/");
+  tft.print((int)getTargetTemp());
+  tft.write(0xF8);
+  tft.print("C  ");
+
+  bool isSwitchOn = setRelay(false);
 
   // this is mildly inefficient but the RP2040 has more than enough oomph
   tft.setCursor(0, SCREEN_HEIGHT - TEXT_HEIGHT);
   tft.print((millis() - startTime) / 1000);
-  tft.drawPixel((millis() - startTime) / 1000 * PpSX, zeroY - (temperature - temprange[0]) * PpDY, ILI9341_YELLOW);
+  tft.drawPixel((millis() - startTime) / 1000 * PpSX, zeroY - (temperature - temprange[0]) * PpDY, isSwitchOn ? ILI9341_GREEN : ILI9341_RED);
+
+// also print it to serial for debug purposes
+#if (DEBUG)
+  Serial.print((millis() - startTime) / 1000);
+  Serial.print("\tTemperature: ");
+  Serial.print(temperature);
+  Serial.print("\tTarget: ");
+  Serial.print(getTargetTemp());
+  Serial.print("\tStage: ");
+  Serial.print(currentStage);
+  Serial.print("\t");
+  Serial.println(isSwitchOn);
+#endif
 }
 
 void finishedReflow()
@@ -297,13 +298,76 @@ void finishedReflow()
   tft.print("s");
   while (1)
   {
+    thermoCouple.read();
+    temperature = thermoCouple.getCelsius();
+    tft.drawPixel((millis() - startTime) / 1000 * PpSX, zeroY - (temperature - temprange[0]) * PpDY, ILI9341_RED);
+    delay(1.0 / PpSX * 500); // run every half of the pixel interval to ensure every point gets logged
   }
 }
 
-float getTargetTemp()
+float getTargetTemp(int futureTime)
 {
+  int checkTime = (millis() - startTime) / 1000 + futureTime;
   float tempTarget;
-  tempTarget = temps[currentStage];
+
+  // TODO: FIX for if there's more than 1 stage skip
+  // not that it's incredibly necessary
+  if (checkTime > times[currentStage])
+  {
+    tempTarget = temps[currentStage + 1];
+  }
+  else
+  {
+    tempTarget = temps[currentStage];
+  }
 
   return tempTarget;
+}
+
+bool setRelay(bool holdTemp, int tempToHold)
+{
+  int switchState = digitalRead(RELAYPIN);
+  bool isHeatNeeded = false;
+  int targetTemp;
+
+  if (holdTemp)
+  {
+    targetTemp = tempToHold;
+    if (temperature < targetTemp)
+    {
+      isHeatNeeded = true;
+    }
+  }
+  else
+  {
+    for (int i = 0; i < CALIBRATE_SECONDS; i += 5)
+    {
+      targetTemp = getTargetTemp(i);
+
+      // Add calibration offset for future prediction
+      float predictedTemp = temperature + (CALIBRATE_TEMP * i / CALIBRATE_SECONDS);
+
+      if (predictedTemp < targetTemp)
+      {
+        isHeatNeeded = true;
+        break;
+      }
+    }
+  }
+
+  // if enough time has passed since the last time we switched AND a switch is needed, make it
+  if (isHeatNeeded && !switchState && millis() > lastSwitch + SWITCH_DELAY)
+  {
+    digitalWrite(RELAYPIN, HIGH);
+    switchState = 1;
+    lastSwitch = millis();
+  }
+  else if (!isHeatNeeded && switchState && millis() > lastSwitch + SWITCH_DELAY)
+  {
+    digitalWrite(RELAYPIN, LOW);
+    switchState = 0;
+    lastSwitch = millis();
+  }
+
+  return switchState;
 }
